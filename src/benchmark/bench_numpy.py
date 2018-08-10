03d87089fe4430f5c44f81160da2821612e7b2ef
mir-am <mir-am@hotmail.com>
2018-08-10 20:13:48 +0430

    A simple NumPy's Dense Matrix Multiplication benchmark.

---
+++ src/benchmark/bench_numpy.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Date: Aug 10 17:15:44 2018
+Developer: Mir, A.
+
+Python's NumPy benchmark
+ver: 0.1
+
+This module is created for benchmarking the performance of NumPy package installed
+on system.
+"""
+
+import numpy as np
+import platform
+import psutil
+import functools
+import timeit
+
+
+# Program constants
+__version__ = '0.1'
+DASHED_LINES = 35 * "-"
+rep_bench = 3 # number of times that a test will be performed
+
+program_header = """
+Python's NumPy benchmark v. %s
+Developer: Mir, A.
+%s
+""" % (__version__, DASHED_LINES)
+
+
+# Functions definition
+
+def blas_info():
+    
+    """
+    Returns BLAS library which is linked with NumPy package on user system
+    """
+    
+    blas_libs_name = ('blas_mkl', 'blas', 'openblas', 'blas_opt', 'lapack_mkl',\
+                      'openblas_lapack', 'lapack_opt')
+    
+    for lib in blas_libs_name:
+        
+        blas_lib_info = np.__config__.get_info(lib)
+        
+        if bool(blas_lib_info) == True:
+            
+            return blas_lib_info['libraries'][0]
+            
+
+def sys_info():
+    
+    """
+    Prints user's system info such as CPU model, RAM, OS, Numpy Version and etc.
+    """
+    
+    # Software info such OS name and Python version
+    numpy_ver = "%s (Linked with %s)" % (np.__version__, blas_info())
+    python_ver = platform.python_version()
+    os_info = "%s %s" % (platform.system(), platform.release())
+    
+    # Hardware info
+    cpu_info = "(%s Cores, %.2f GHz)" % (psutil.cpu_count(), psutil.cpu_freq().max / 1000)
+    total_memory = "%d MB" % int(psutil.virtual_memory().total / 1024 / 1024)
+    
+    sys_info_msg = """
+System Info:
+Python Version: %s
+NumPy Version: %s 
+OS: %s
+CPU: %s
+RAM: %s
+%s
+"""
+    
+    return sys_info_msg % (python_ver, numpy_ver, os_info, cpu_info, total_memory, \
+                           DASHED_LINES)
+    
+    
+def start_bench():
+    
+    """
+    NumPy's benchmark
+    Tests with different matrix sizes (512, 1024, 2048, 4096, 8192)
+    Performs matrix multiplication
+    """
+    
+    mat_sizes = (512, 1024, 2048, 4096, 8192)
+    
+    test_str = "Test %d: Matrix of size %d x %d - Time: %.3f Seconds"
+    
+    for t, size in enumerate(mat_sizes):
+        
+        # Create two random square matrix for multiplication
+        mat_A = np.random.rand(size, size)
+        mat_B = np.random.rand(size, size)
+        
+        bench_time = round(timeit.Timer(functools.partial(np.dot, mat_A, \
+                           mat_B)).timeit(rep_bench) / rep_bench, 3)
+        
+        print(test_str % (t + 1, mat_A.shape[0], mat_B.shape[0], bench_time))
+
+ 
+    
+        
+    
+    
+
+########################
+
+
+if __name__ == '__main__':
+    
+    print(program_header)
+    
+    print(sys_info())
+    
+    input("Press Enter to start the benchmark... \n")
+    
+    start_bench()
